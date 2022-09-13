6feace4f8cfcbae60ef6f851bc75183e0d1cba0e
Hidayet <hidayetozsoy@outlook.com>
2022-09-14 01:34:32 +0300

    addresses comment added

--- src/utils/config.py
+++ src/utils/config.py
@@ -6,7 +6,7 @@ PRIVATE_KEYS = { #address : private key
     "0x4043D2508C591f57C1C0d36CF227be5a3ace83d4":"686e075ee398432d1ea14f9acabd93e695f77404bc51d800dab3c0c1aa02fe14",
 }
 
-ADDRESSES = {
+ADDRESSES = { # order of addresses on Metamask extension. The order MUST be the same with order in Metamask.
     "0x82A323000D50CABaEd045A71FDE88e01a8b082d8":"1",
     "0x4043D2508C591f57C1C0d36CF227be5a3ace83d4":"2",
 }
